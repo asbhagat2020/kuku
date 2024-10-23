46b48da2a612fb961dc6416104fefb369d745133
soumyadeepdutta7 <soumyadeepdutta.sd@gmail.com>
2024-10-23 16:14:50 +0530

    Bug fixed for Home, Profile , Brand ,Animation and category section

--- components/home/AnimationSection.jsx
+++ components/home/AnimationSection.jsx
@@ -4,24 +4,31 @@ import React from 'react'
 const AnimationSection = () => {
   return (
     <div className="max-w-[1550px] mx-auto mb-[93px] relative">
-        <div className='absolute top-[-80px] right-[80px]'>
-            <Image src='list_top_image.svg' width={111} height={121} alt=''/>
-        </div>
-        <div className='rounded-[20px] mx-5 lg:mx-[70px] lg:h-[640px] bg-[#FDE504] py-[60px] flex flex-col items-center'>
-            <div className='flex mx-[180px] gap-[130px]'>
-                <div className='flex flex-col space-y-[50px] justify-center'>
-                    <p className='text-[#202020] text-base font-bold font-karla leading-tight'>Step-1</p>
-                    <h1 className='text-[#e4086f] lg:text-[46px] font-normal font-luckiest lg:leading-[55.20px]'>List what you no <br /> longer need</h1>
-                </div>
-                <div>
-                    <Image width={405} height={405} src='/mobile.png' alt=''/>
-                </div>
-            </div>
-            <div className='mt-[38px] mx-7'>
-            <Image unoptimized width={1200} height={57} src='/animation_slide_image1.png' alt=''/>
-            </div>
-        </div>
+  <div className="absolute top-[-50px] right-[20px] sm:top-[-80px] sm:right-[80px]">
+    <Image src="list_top_image.svg" width={111} height={121} alt="" />
+  </div>
+  
+  <div className="rounded-[20px] mx-5 lg:mx-[70px] lg:h-[640px] bg-[#FDE504] py-[40px] sm:py-[60px] flex flex-col items-center">
+    
+    <div className="flex flex-col sm:flex-row mx-5 sm:mx-[50px] lg:mx-[180px] gap-[30px] sm:gap-[130px] items-center">
+      <div className="flex flex-col space-y-5 sm:space-y-[50px] justify-center text-center sm:text-left">
+        <p className="text-[#202020] text-sm sm:text-base font-bold font-karla leading-tight">Step-1</p>
+        <h1 className="text-[#e4086f] text-[26px] sm:text-[32px] lg:text-[46px] font-normal font-luckiest leading-tight sm:leading-[42px] lg:leading-[55.20px]">
+          List what you no <br /> longer need
+        </h1>
+      </div>
+
+      <div className="mt-5 sm:mt-0">
+        <Image width={250} height={250} className="sm:w-[305px] sm:h-[305px] lg:w-[405px] lg:h-[405px]" src="/mobile.png" alt="" />
+      </div>
     </div>
+    
+    <div className="mt-5 sm:mt-[38px] mx-2 sm:mx-7">
+      <Image unoptimized width={300} height={30} className="sm:w-[600px] sm:h-[70px] lg:w-[1200px] lg:h-[90px]" src="/animation_slide_image1.png" alt="" />
+    </div>
+  </div>
+</div>
+
   )
 }
 
--- components/home/CategoryCarousel.jsx
+++ components/home/CategoryCarousel.jsx
@@ -1,171 +1,228 @@
-"use client"
-import Image from 'next/image';
-import React, { useState, useRef, useEffect } from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import { motion } from 'framer-motion'
+"use client";
+import Image from "next/image";
+import React, { useState, useRef, useEffect } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { motion } from "framer-motion";
 
 const CategoryCarousel = () => {
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const [centerIndex, setCenterIndex] = useState(0);
-    const sliderRef = useRef(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [centerIndex, setCenterIndex] = useState(0);
+  const sliderRef = useRef(null);
 
-    const products = [
-        { title: "Dress", price: "12 AED", image: "/top.png" },
-        { title: "Dress", price: "12 AED", image: "/bottom.png" },
-        { title: "Dress", price: "12 AED", image: "/t-shirt.png" },
-        { title: "Dress", price: "12 AED", image: "/top.png" },
-        { title: "Dress", price: "12 AED", image: "/bottom.png" },
-        { title: "Dress", price: "12 AED", image: "/t-shirt.png" },
-        { title: "Dress", price: "12 AED", image: "/top.png" },
-        { title: "Dress", price: "12 AED", image: "/bottom.png" },
-        { title: "Dress", price: "12 AED", image: "/t-shirt.png" },
-        { title: "Dress", price: "12 AED", image: "/t-shirt.png" },
-    ];
+  const products = [
+    { title: "Dress", price: "12 AED", image: "/top.png" },
+    { title: "Dress", price: "12 AED", image: "/bottom.png" },
+    { title: "Dress", price: "12 AED", image: "/t-shirt.png" },
+    { title: "Dress", price: "12 AED", image: "/top.png" },
+    { title: "Dress", price: "12 AED", image: "/bottom.png" },
+    { title: "Dress", price: "12 AED", image: "/t-shirt.png" },
+    { title: "Dress", price: "12 AED", image: "/top.png" },
+    { title: "Dress", price: "12 AED", image: "/bottom.png" },
+    { title: "Dress", price: "12 AED", image: "/t-shirt.png" },
+    { title: "Dress", price: "12 AED", image: "/t-shirt.png" },
+  ];
 
-    const settings = {
-        dots: false,
-        arrows: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4.36,
-        slidesToScroll: 1,
-        centerMode: true,
-        centerPadding: '0px',
-        beforeChange: (current, next) => setCurrentSlide(next),
-        responsive: [
-            {
-                breakpoint: 1280,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2.4,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ],
-    };
-    const [isLeftHovered, setIsLeftHovered] = useState(false);
-    const [isRightHovered, setIsRightHovered] = useState(false);
+  const settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4.36,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "0px",
+    beforeChange: (current, next) => setCurrentSlide(next),
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2.4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  const [isLeftHovered, setIsLeftHovered] = useState(false);
+  const [isRightHovered, setIsRightHovered] = useState(false);
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Update centerIndex when window is resized
+      const handleResize = () => {
+        const slidesToShow =
+          window.innerWidth < 480
+            ? 1
+            : window.innerWidth < 600
+            ? 1
+            : window.innerWidth < 1024
+            ? 1
+            : 4;
+        setCenterIndex(Math.floor(slidesToShow / 4));
+      };
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            // Update centerIndex when window is resized
-            const handleResize = () => {
-                const slidesToShow = window.innerWidth < 480 ? 1 : window.innerWidth < 600 ? 1 : window.innerWidth < 1024 ? 1 : 4;
-                setCenterIndex(Math.floor(slidesToShow / 4));
-            };
+      handleResize(); // Initial call
+      window.addEventListener("resize", handleResize);
 
-            handleResize(); // Initial call
-            window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []);
 
-            return () => window.removeEventListener('resize', handleResize);
-        }
-    }, []);
+  const handlePreviousSlide = () => {
+    if (sliderRef.current) {
+      sliderRef.current.slickPrev();
+    }
+  };
 
+  const handleNextSlide = () => {
+    if (sliderRef.current) {
+      sliderRef.current.slickNext();
+    }
+  };
 
-    const handlePreviousSlide = () => {
-        if (sliderRef.current) {
-            sliderRef.current.slickPrev();
-        }
-    };
-
-    const handleNextSlide = () => {
-        if (sliderRef.current) {
-            sliderRef.current.slickNext();
-        }
-    };
-
-    return (
-        <div className="flex flex-col overflow-hidden px-[20px] md:px-0 ">
-            <Slider ref={sliderRef} {...settings}>
-                {products.map((item, index) => (
-                    <div className='relative' key={index}>
-                        {index !== currentSlide && (
-                            <div className="absolute top-2 left-10 md:left-[40px] h-[25px] px-2.5 py-[5px] bg-[#e4086f]/10 rounded-[10px] justify-center items-center gap-2.5 inline-flex">
-                                <div className="text-black text-xs font-normal font-karla">Top</div>
-                            </div>
-                        )}
-                        <div
-                            className={`w-[300px] h-[386px] mx-8 bg-white transition-all duration-500 ${index === currentSlide  ? "shadow-lg rounded-[155px]" : "rounded-[20px]"
-                                }`}
-                        >
-                            <Image
-                                src={item.image}
-                                width={307}
-                                height={307}
-                                layout="responsive"
-                                alt={item.title}
-                                className='w-[307px] '
-                            />
-                        </div>
-                    </div>
-                ))}
-            </Slider>
-            <div className='flex mt-[76px] justify-center items-center gap-7'>
-                <motion.div
-                    whileHover={{
-                        scale: 1.2,
-                        backgroundColor: 'black',
-                    }}
-                    className='rounded-full w-[60px] h-[60px] bg-white flex justify-center items-center cursor-pointer'
-                    onClick={handlePreviousSlide}
-                    onMouseEnter={() => setIsLeftHovered(true)}
-                    onMouseLeave={() => setIsLeftHovered(false)}
-                >
-                    <Image
-                        width={18}
-                        height={18}
-                        src={isLeftHovered ? '/arrow_left_white.svg' : '/arrow_left.png'}
-                        alt='Previous'
-                    />
-                </motion.div>
-                <div className="h-[84px] px-[70px] py-[30px] bg-[#f0fafe] rounded-[20px] justify-center items-center gap-2.5 inline-flex">
-                    <div className="text-[#070707] lg:text-xl font-bold font-karla leading-normal">
-                        T-SHIRT
-                    </div>
+  return (
+    <div className="flex flex-col overflow-hidden px-[20px] md:px-0 ">
+      <Slider ref={sliderRef} {...settings}>
+        {products.map((item, index) => (
+          <div className="relative" key={index}>
+            {index !== currentSlide && (
+              <div className="absolute top-2 left-10 md:left-[40px] h-[25px] px-2.5 py-[5px] bg-[#e4086f]/10 rounded-[10px] justify-center items-center gap-2.5 inline-flex">
+                <div className="text-black text-xs font-normal font-karla">
+                  Top
                 </div>
-                <motion.div
-                    whileHover={{
-                        scale: 1.2,
-                        backgroundColor: 'black',
-                    }}
-                    className='rounded-full w-[60px] bg-[#f0fafe] h-[60px] flex justify-center items-center cursor-pointer'
-                    onClick={handleNextSlide}
-                    onMouseEnter={() => setIsRightHovered(true)}
-                    onMouseLeave={() => setIsRightHovered(false)}
-                >
-                    <Image
-                        width={18}
-                        height={18}
-                        src={isRightHovered ? '/arrow_right_white.svg' : '/arrow_right_black.svg'}
-                        alt='Next'
-                    />
-                </motion.div>
+              </div>
+            )}
+            <div
+              className={`w-[300px] h-[386px] mx-8 bg-white transition-all duration-500 ${
+                index === currentSlide
+                  ? "shadow-lg rounded-[155px]"
+                  : "rounded-[20px]"
+              }`}
+            >
+              <Image
+                src={item.image}
+                width={307}
+                height={307}
+                layout="responsive"
+                alt={item.title}
+                className="w-[307px] "
+              />
             </div>
+          </div>
+        ))}
+      </Slider>
+      <div className="flex mt-[76px] justify-center items-center gap-7">
+        {/* Mobile Arrow Buttons */}
+        <div className="sm:hidden flex items-center">
+          <div
+            className="rounded-full w-[60px] h-[60px] bg-white flex justify-center items-center cursor-pointer mr-4"
+            onClick={handlePreviousSlide}
+          >
+            <Image
+              width={18}
+              height={18}
+              src="/arrow_left.png"
+              alt="Previous"
+            />
+          </div>
+
+          {/* Central T-Shirt Section */}
+          <div className="h-[84px] px-4 py-4 bg-[#f0fafe] rounded-[20px] flex justify-center items-center gap-2.5">
+            <div className="text-[#070707] text-base sm:text-xl font-bold font-karla leading-normal">
+              T-SHIRT
+            </div>
+          </div>
+
+          <div
+            className="rounded-full w-[60px] h-[60px] bg-[#f0fafe] flex justify-center items-center cursor-pointer ml-4"
+            onClick={handleNextSlide}
+          >
+            <Image
+              width={18}
+              height={18}
+              src="/arrow_right_black.svg"
+              alt="Next"
+            />
+          </div>
         </div>
-    );
+
+        {/* Left Arrow Button */}
+        <div className="hidden sm:flex">
+          <motion.div
+            whileHover={{
+              scale: 1.2,
+              backgroundColor: "black",
+            }}
+            className="rounded-full w-[60px] h-[60px] bg-white flex justify-center items-center cursor-pointer"
+            onClick={handlePreviousSlide}
+            onMouseEnter={() => setIsLeftHovered(true)}
+            onMouseLeave={() => setIsLeftHovered(false)}
+          >
+            <Image
+              width={18}
+              height={18}
+              src={isLeftHovered ? "/arrow_left_white.svg" : "/arrow_left.png"}
+              alt="Previous"
+            />
+          </motion.div>
+        </div>
+
+        {/* Central T-Shirt Section for larger screens */}
+        <div className="hidden sm:flex h-[84px] px-4 sm:px-[70px] py-4 sm:py-[30px] bg-[#f0fafe] rounded-[20px] flex justify-center items-center gap-2.5">
+          <div className="text-[#070707] text-base sm:text-xl font-bold font-karla leading-normal">
+            T-SHIRT
+          </div>
+        </div>
+
+        {/* Right Arrow Button */}
+        <div className="hidden sm:flex">
+          <motion.div
+            whileHover={{
+              scale: 1.2,
+              backgroundColor: "black",
+            }}
+            className="rounded-full w-[60px] bg-[#f0fafe] h-[60px] flex justify-center items-center cursor-pointer"
+            onClick={handleNextSlide}
+            onMouseEnter={() => setIsRightHovered(true)}
+            onMouseLeave={() => setIsRightHovered(false)}
+          >
+            <Image
+              width={18}
+              height={18}
+              src={
+                isRightHovered
+                  ? "/arrow_right_white.svg"
+                  : "/arrow_right_black.svg"
+              }
+              alt="Next"
+            />
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryCarousel;
