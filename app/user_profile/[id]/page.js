624caeb8ce3b7c9acab4b82737f15ac84bf54d05
soumyadeepdutta7 <soumyadeepdutta.sd@gmail.com>
2025-01-08 16:15:13 +0530

    user profile sign out fixed

--- app/user_profile/[id]/page.js
+++ app/user_profile/[id]/page.js
@@ -3,23 +3,28 @@ import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import DetailsSection from '@/components/userProfile/DetailsSection';
 import ProfileSection from '@/components/userProfile/ProfileSection';
-import ReviewItem from '@/components/userProfile/ReviewItem';
-
+import { useRouter } from 'next/navigation';
 import { useSelector } from "react-redux";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "next/navigation";
 import Cookies from 'js-cookie';
 
-
 const Page = () => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   const { id } = useParams();
- 
+  const router = useRouter();
 
   useEffect(() => {
+    const token = Cookies.get('auth');
+    if (!token) {
+      // If no token, redirect to homepage
+      router.push('/');
+      return;
+    }
+
     if (id) {
       fetchUser();
     }
@@ -27,22 +32,24 @@ const Page = () => {
 
   const fetchUser = async () => {
     try {
-    const token = JSON.parse(Cookies.get('auth'));
+      const token = JSON.parse(Cookies.get('auth'));
 
       const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/profile/details/${id}`;
-     
-      
-    
-      const response = await axios.get(url ,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
 
       setData(response.data.profile);
     } catch (err) {
-      setError("Failed to fetch user details");
+      if (err.response && err.response.status === 401) {
+        // If unauthorized, clear token and redirect to homepage
+        Cookies.remove('auth');
+        router.push('/');
+      } else {
+        setError("Failed to fetch user details");
+      }
     } finally {
       setLoading(false);
     }
