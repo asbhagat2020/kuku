5a1af30f26c77b235655fed99b5f7d716f745b25
ashwinbt <ashwinbt.moshimoshi@gmail.com>
2025-09-30 18:39:19 +0530

    fixed

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,28 @@ const nextConfig = {
    images: {
     unoptimized: true, // disables Next.js image optimization, avoids using sharp
   },
+
+   async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://translate.google.com https://translate.googleapis.com",
+              "style-src 'self' 'unsafe-inline' https://translate.googleapis.com",
+              "img-src 'self' data: https: https://translate.googleapis.com",
+              "font-src 'self' data: https://fonts.gstatic.com",
+              "connect-src 'self' https://translate.googleapis.com https://translate.google.com",
+              "frame-src https://translate.google.com https://translate.googleapis.com",
+            ].join('; '),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
