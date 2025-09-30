aeea70c0150e97fafed2b6471487cdeee474f3a8
ashwinbt <ashwinbt.moshimoshi@gmail.com>
2025-09-30 17:59:56 +0530

    fixed

--- components/GoogleTranslate.jsx
+++ components/GoogleTranslate.jsx
@@ -431,3 +431,9 @@ export default function GoogleTranslate() {
 
 
 
+
+
+
+
+
+
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["example.com","kuku.moshimoshi.cloud","s3.ap-south-1.amazonaws.com"],
+    domains: ["kuku.moshimoshi.cloud","s3.ap-south-1.amazonaws.com"],
   },
    images: {
     unoptimized: true, // disables Next.js image optimization, avoids using sharp
